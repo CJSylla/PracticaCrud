63eb2f03242e445e640e220ddc884af26f90bbdc
Claude Junior Sylla <cjsylla93@gmail.com>
2019-12-06 12:46:25 -0400

    Commit

---
+++ src/main/java/edu/pucmm/tarea_clase_crud_springboot/CrudTarea.java
@@ -0,0 +1,13 @@
+package edu.pucmm.tarea_clase_crud_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTarea {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTarea.class, args);
+    }
+
+}
---
+++ src/test/java/com/Practica/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
